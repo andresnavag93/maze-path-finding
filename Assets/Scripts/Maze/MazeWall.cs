0851bddd177464776f878ab8c3979c1383e4bf65
Andres Navarro <andresnavag93@gmail.com>
2022-02-05 09:57:34 -0400

    :hammer: MazeCell class developed

---
+++ Assets/Scripts/Maze/MazeWall.cs
@@ -0,0 +1,12 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+/// <summary>
+/// Class MazeWall 
+/// </summary>
+public class MazeWall : MazeCellEdge
+{
+
+}
+
